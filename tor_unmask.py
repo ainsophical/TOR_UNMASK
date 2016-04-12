5c41728051edcf7fcd8802fbfcb2c12842c9cd6f
Michael G <Michael@sofdigital.net>
2016-04-12 03:42:56 -0400

    clean up

--- tor_unmask.py
+++ tor_unmask.py
@@ -1,9 +1,10 @@
-def IsTorExitNode(request):
-	if gethostbyname(ReverseIPOctets(request.META.get('REMOTE_ADDR')))+'.'+request.META.get('SERVER_PORT')+'.'+ReverseIPOctets(request.META.get('SERVER_ADDR'))+'.'+'.ip-port.exitlist.torproject.org' == '127.0.0.2':
-		return True
-	else:
-		return False
+def checktor(request):
+    tordnsel = revip(request.META.get('REMOTE_ADDR'))+'.'+request.META.get('SERVER_PORT')+'.'+revoct(request.META.get('SERVER_ADDR'))
+    if gethostbyname(str(tordnsel)+'.ip-port.exitlist.torproject.org') == '127.0.0.2':
+        return True
+    else:
+        return False
 
-def ReverseIPOctets(inputip):
-	ipoc = inputip.split('.')
-	return ipoc[3]+'.'+ipoc[2]+'.'+ipoc[1]+'.'+ipoc[0]
+def revip(ip):
+    ip = '.'.join(reversed(ip.split('.')))
+    return str(ip)
