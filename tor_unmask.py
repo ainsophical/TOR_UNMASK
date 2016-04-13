c6fe0964da4387f8ab62367519c9e444e03251e1
Michael G <Michael@sofdigital.net>
2016-04-12 22:58:15 -0400

    commit

--- tor_unmask.py
+++ tor_unmask.py
@@ -1,8 +1,12 @@
+from socket import gethostbyname
+
 def checktor(request):
     def revip(ip):
 	    ip = '.'.join(reversed(ip.split('.')))
 	    return str(ip)
-    tordnsel = revip(request.META.get('REMOTE_ADDR'))+'.'+request.META.get('SERVER_PORT')+'.'+revoct(request.META.get('SERVER_ADDR'))
+    tordnsel = '.'.join([revip(request.META.get('REMOTE_ADDR')),
+    					 request.META.get('SERVER_PORT'),
+    					 revip(request.META.get('SERVER_ADDR'))])
     if gethostbyname(str(tordnsel)+'.ip-port.exitlist.torproject.org') == '127.0.0.2':
         return True
     else:
