23cea2f1c1c9e69ecf01cb1cf2421d69b860d302
michael <michael@sofdigital.net>
2016-04-12 22:59:24 -0400

    Update tor_unmask.py

--- tor_unmask.py
+++ tor_unmask.py
@@ -5,8 +5,8 @@ def checktor(request):
 	    ip = '.'.join(reversed(ip.split('.')))
 	    return str(ip)
     tordnsel = '.'.join([revip(request.META.get('REMOTE_ADDR')),
-    					 request.META.get('SERVER_PORT'),
-    					 revip(request.META.get('SERVER_ADDR'))])
+    		         request.META.get('SERVER_PORT'),
+    		         revip(request.META.get('SERVER_ADDR'))])
     if gethostbyname(str(tordnsel)+'.ip-port.exitlist.torproject.org') == '127.0.0.2':
         return True
     else:
