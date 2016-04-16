3e2b614b11a7f807236d22a9ad3596abba7fca8d
Michael G <Michael@sofdigital.net>
2016-04-16 00:21:57 -0400

    converted to decorator

--- tor_unmask.py
+++ tor_unmask.py
@@ -1,14 +1,20 @@
 from socket import gethostbyname
 
-def checktor(request):
-    def revip(ip):
-	    ip = '.'.join(reversed(ip.split('.')))
-	    return str(ip)
-    tordnsel = '.'.join([revip(request.META.get('REMOTE_ADDR')),
-    		         request.META.get('SERVER_PORT'),
-    		         revip(request.META.get('SERVER_ADDR'))])
-    if gethostbyname(str(tordnsel)+'.ip-port.exitlist.torproject.org') == '127.0.0.2':
-        return True
-    else:
-        return False
+def tor(f):
+    @wraps(f)
+    def checktor(request, *args, **kwargs):
+        def revip(ip):
+            ip = '.'.join(reversed(ip.split('.')))
+            return str(ip)
+        tordnsel = '.'.join([revip(request.META.get('REMOTE_ADDR')),
+                             request.META.get('SERVER_PORT'),
+                             revip(request.META.get('SERVER_ADDR'))])
+        if gethostbyname(str(tordnsel)+'.ip-port.exitlist.torproject.org') == '127.0.0.2':
+            request.META['tor'] = 'Enabled'
+            return f(request, *args, **kwargs)
+        else:
+            request.META['tor'] = 'Not Enabled'
+            return 'Not Enabled'
+    return checktor
+
 
